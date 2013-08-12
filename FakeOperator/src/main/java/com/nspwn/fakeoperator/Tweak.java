e37e49012a02266870bb5488ff732b5c7b45a3fe
Jordan <greysyntax@nspwn.com>
2013-08-12 21:20:44 +0100

    FakeOperator/build.gradle: Fixed building with substrate's blessed classloaded FakeOperator/src/main/java/com/nspwn/fakeoperator/Tweak.java: Correctly hook NetworkController#updateNetworkName

--- FakeOperator/src/main/java/com/nspwn/fakeoperator/Tweak.java
+++ FakeOperator/src/main/java/com/nspwn/fakeoperator/Tweak.java
@@ -4,7 +4,6 @@ import android.telephony.ServiceState;
 import android.telephony.TelephonyManager;
 import android.util.Log;
 
-import com.android.systemui.statusbar.policy.NetworkController;
 import com.saurik.substrate.MS;
 
 import java.lang.reflect.Method;
@@ -18,8 +17,6 @@ public class Tweak {
         Log.i(TAG, "waiting for class load(s)...");
 
         hookNetworkController();
-//        hookServiceState();
-//        hookTelephonyManager();
     }
 
     private static void hookNetworkController() {
@@ -31,21 +28,21 @@ public class Tweak {
 
                     if (updateNetworkName != null) {
                         Log.d(TAG, "hooking updateNetworkName");
-
                         //updateNetworkName(boolean showSpn, String spn, boolean showPlmn, String plmn) {
-                        MS.hookMethod(resources, updateNetworkName, new MS.MethodAlteration<NetworkController, Void>() {
-                            public Void invoked(NetworkController networkController, Object... args) throws Throwable {
+
+                        MS.hookMethod(resources, updateNetworkName, new MS.MethodAlteration<Object, Void>() {
+                            public Void invoked(Object networkController, Object... args) throws Throwable {
                                 Log.d(TAG, "running updateNetworkName");
                                 boolean showSpn = (Boolean)args[0];
-                                String spn = "SPN";//(String)args[1];
+                                String spn = "NSPwn (@GreySyntax)";//(String)args[1];
                                 boolean showPlmn = (Boolean)args[2];
                                 String plmn = "PLMN";//(String)args[3];
 
                                 if (BuildConfig.DEBUG) {
-                                    Log.d("CarrierLabel", String.format("updateNetworkName showSpn=%b  spn=%s showPlmn=%b plmn=%s", showSpn, spn, showPlmn, plmn));
+                                    Log.d(TAG, String.format("updateNetworkName showSpn=%b  spn=%s showPlmn=%b plmn=%s", showSpn, spn, showPlmn, plmn));
                                 }
 
-                                return invoke(networkController, showSpn, spn, showPlmn, plmn);
+                                return invoke(networkController,  showSpn, spn, showPlmn, plmn);
                             }
                         });
                     }
@@ -56,99 +53,99 @@ public class Tweak {
         });
     }
 
-//    private static void hookTelephonyManager() {
-//        MS.hookClassLoad("android.telephony.TelephonyManager", new MS.ClassLoadHook() {
-//            public void classLoaded(Class<?> resources) {
-//                try {
-//                    Log.d(TAG, "hooking getNetworkOperatorName");
-//                    Method getNetworkOperatorName = resources.getMethod("getNetworkOperatorName");
-//
-//                    if (getNetworkOperatorName != null) {
-//                        Log.d(TAG, "got getNetworkOperatorName");
-//                        MS.hookMethod(resources, getNetworkOperatorName, new MS.MethodAlteration<TelephonyManager, String>() {
-//                            public String invoked(TelephonyManager telephonyManager, Object... args) throws Throwable {
-//                                String real = invoke(telephonyManager, args);
-//                                Log.d(TAG, String.format("running getNetworkOperatorName (real: %s)", real));
-//
-//                                if (real != null) {
-//                                    return "TEST";// String.format("FO:A - %s", real);
-//                                }
-//
-//                                return null;
-//                            }
-//                        });
-//                    }
-//
-//                    Log.d(TAG, "hooking getSimOperatorName");
-//                    Method getSimOperatorName = resources.getMethod("getSimOperatorName");
-//
-//                    if (getSimOperatorName != null) {
-//                        Log.d(TAG, "got getSimOperatorName");
-//                        MS.hookMethod(resources, getSimOperatorName, new MS.MethodAlteration<TelephonyManager, String>() {
-//                            public String invoked(TelephonyManager telephonyManager, Object... args) throws Throwable {
-//                                String real = invoke(telephonyManager, args);
-//                                Log.d(TAG, String.format("running getSimOperatorName (real: %s)", real));
-//
-//                                if (real != null) {
-//                                    return "TEST";// String.format("FO:A - %s", real);
-//                                }
-//
-//                                return null;
-//                            }
-//                        });
-//                    }
-//                } catch (NoSuchMethodException e) {
-//                    Log.e(TAG, "error hooking method...", e);
-//                }
-//            }
-//        });
-//    }
-//
-//    private static void hookServiceState() {
-//        MS.hookClassLoad("android.telephony.ServiceState", new MS.ClassLoadHook() {
-//            public void classLoaded(Class<?> resources) {
-//                try {
-//                    Log.d(TAG, "hooking getOperatorAlphaLong");
-//                    Method getOperatorAlphaLong = resources.getMethod("getOperatorAlphaLong");
-//
-//                    if (getOperatorAlphaLong != null) {
-//                        Log.d(TAG, "got getOperatorAlphaLong");
-//
-//                        MS.hookMethod(resources, getOperatorAlphaLong, new MS.MethodAlteration<ServiceState, String>() {
-//                            public String invoked(ServiceState serviceState, Object... args) throws Throwable {
-//                                String real = invoke(serviceState, args);
-//                                Log.d(TAG, String.format("running getOperatorAlphaLong (real: %s)", real));
-//
-//                                if (real != null) {
-//                                    return "TEST";// String.format("FO:A - %s", real);
-//                                }
-//
-//                                return null;
-//                            }
-//                        });
-//                    }
-//
-//                    Method getOperatorAlphaShort = resources.getMethod("getOperatorAlphaShort");
-//                    if (getOperatorAlphaShort != null) {
-//                        Log.i(TAG, "got getOperatorAlphaShort");
-//
-//                        MS.hookMethod(resources, getOperatorAlphaShort, new MS.MethodAlteration<ServiceState, String>() {
-//                            public String invoked(ServiceState serviceState, Object... args) throws Throwable {
-//                                String real = invoke(serviceState, args);
-//                                Log.d(TAG, String.format("running getOperatorAlphaShort (real: %s)", real));
-//
-//                                if (real != null) {
-//                                    return "TEST";// String.format("FO:A - %s", real);
-//                                }
-//
-//                                return null;
-//                            }
-//                        });
-//                    }
-//                } catch (NoSuchMethodException e) {
-//                    Log.e(TAG, "error hooking method...", e);
-//                }
-//            }
-//        });
-//    }
+    private static void hookTelephonyManager() {
+        MS.hookClassLoad("android.telephony.TelephonyManager", new MS.ClassLoadHook() {
+            public void classLoaded(Class<?> resources) {
+                try {
+                    Log.d(TAG, "hooking getNetworkOperatorName");
+                    Method getNetworkOperatorName = resources.getMethod("getNetworkOperatorName");
+
+                    if (getNetworkOperatorName != null) {
+                        Log.d(TAG, "got getNetworkOperatorName");
+                        MS.hookMethod(resources, getNetworkOperatorName, new MS.MethodAlteration<TelephonyManager, String>() {
+                            public String invoked(TelephonyManager telephonyManager, Object... args) throws Throwable {
+                                String real = invoke(telephonyManager, args);
+                                Log.d(TAG, String.format("running getNetworkOperatorName (real: %s)", real));
+
+                                if (real != null) {
+                                    return "TEST";// String.format("FO:A - %s", real);
+                                }
+
+                                return null;
+                            }
+                        });
+                    }
+
+                    Log.d(TAG, "hooking getSimOperatorName");
+                    Method getSimOperatorName = resources.getMethod("getSimOperatorName");
+
+                    if (getSimOperatorName != null) {
+                        Log.d(TAG, "got getSimOperatorName");
+                        MS.hookMethod(resources, getSimOperatorName, new MS.MethodAlteration<TelephonyManager, String>() {
+                            public String invoked(TelephonyManager telephonyManager, Object... args) throws Throwable {
+                                String real = invoke(telephonyManager, args);
+                                Log.d(TAG, String.format("running getSimOperatorName (real: %s)", real));
+
+                                if (real != null) {
+                                    return "TEST";// String.format("FO:A - %s", real);
+                                }
+
+                                return null;
+                            }
+                        });
+                    }
+                } catch (NoSuchMethodException e) {
+                    Log.e(TAG, "error hooking method...", e);
+                }
+            }
+        });
+    }
+
+    private static void hookServiceState() {
+        MS.hookClassLoad("android.telephony.ServiceState", new MS.ClassLoadHook() {
+            public void classLoaded(Class<?> resources) {
+                try {
+                    Log.d(TAG, "hooking getOperatorAlphaLong");
+                    Method getOperatorAlphaLong = resources.getMethod("getOperatorAlphaLong");
+
+                    if (getOperatorAlphaLong != null) {
+                        Log.d(TAG, "got getOperatorAlphaLong");
+
+                        MS.hookMethod(resources, getOperatorAlphaLong, new MS.MethodAlteration<ServiceState, String>() {
+                            public String invoked(ServiceState serviceState, Object... args) throws Throwable {
+                                String real = invoke(serviceState, args);
+                                Log.d(TAG, String.format("running getOperatorAlphaLong (real: %s)", real));
+
+                                if (real != null) {
+                                    return "TEST";// String.format("FO:A - %s", real);
+                                }
+
+                                return null;
+                            }
+                        });
+                    }
+
+                    Method getOperatorAlphaShort = resources.getMethod("getOperatorAlphaShort");
+                    if (getOperatorAlphaShort != null) {
+                        Log.i(TAG, "got getOperatorAlphaShort");
+
+                        MS.hookMethod(resources, getOperatorAlphaShort, new MS.MethodAlteration<ServiceState, String>() {
+                            public String invoked(ServiceState serviceState, Object... args) throws Throwable {
+                                String real = invoke(serviceState, args);
+                                Log.d(TAG, String.format("running getOperatorAlphaShort (real: %s)", real));
+
+                                if (real != null) {
+                                    return "TEST";// String.format("FO:A - %s", real);
+                                }
+
+                                return null;
+                            }
+                        });
+                    }
+                } catch (NoSuchMethodException e) {
+                    Log.e(TAG, "error hooking method...", e);
+                }
+            }
+        });
+    }
 }
